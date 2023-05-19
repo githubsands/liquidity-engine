2c48b82fce8d73be07a3c0cfc71b1b02324839a7
Ryan Vacek <ryanvacek@Ryans-MacBook-Pro.local>
2023-05-18 22:48:45 -0600

    switches to iterators and streams for snapshots

--- market-object/src/lib.rs
+++ market-object/src/lib.rs
@@ -11,7 +11,7 @@ use tokio_tungstenite::tungstenite::protocol::Message;
 // k: 0 ask, 1 bid
 // p: price or level
 // q: quantity
-// l: location: 0 binance, 1 binance_usa, 2 bybit
+// l: location: 1 binance, 2 binance_usa, 3 bybit
 pub struct DepthUpdate {
     pub k: u8,
     pub p: f64,
@@ -210,10 +210,108 @@ impl HTTPSnapShotDepthResponseBinance {
     }
 }
 
-pub fn test(ws_message: Message) -> Result<WSDepthUpdateBinance, Infallible> {
-    let actual: Result<WSDepthUpdateBinance, Infallible> =
-        WSDepthUpdateBinance::try_from(ws_message);
-    return actual;
+#[derive(Debug, Serialize, Deserialize)]
+pub struct WSOrderBookUpdateByBit {
+    pub price: f64,
+    pub quantity: f64,
+}
+
+impl WSDepthUpdateByBit {
+    pub fn depths(
+        self,
+        location: u8,
+    ) -> (
+        impl Iterator<Item = DepthUpdate>,
+        impl Iterator<Item = DepthUpdate>,
+    ) {
+        let bid_updates = self.data.b.into_iter().map(move |update| DepthUpdate {
+            k: 0,
+            p: update.price,    // Assuming BinanceDepthUpdate has a `price` field
+            q: update.quantity, // and a `quantity` field
+            l: location,
+        });
+
+        let ask_updates = self.data.a.into_iter().map(move |update| DepthUpdate {
+            k: 1,
+            p: update.price,
+            q: update.quantity,
+            l: location,
+        });
+
+        (bid_updates, ask_updates)
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct WSDepthUpdateByBit {
+    pub topic: String,
+    pub type_: String,
+    pub ts: u64,
+    pub data: Data,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Data {
+    pub s: String,
+    pub b: Vec<WSOrderBookUpdateByBit>,
+    pub a: Vec<WSOrderBookUpdateByBit>,
+    pub u: u32,
+    pub seq: u32,
+}
+
+impl From<Message> for WSDepthUpdateByBit {
+    fn from(value: Message) -> Self {
+        match value {
+            Message::Text(text) => {
+                serde_json::from_str(&text).expect("Failed to parse the message")
+            }
+            _ => panic!("Expected Text WebSocket Message"),
+        }
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ByBitData {
+    pub s: String,
+    pub t: u64,
+    pub a: Vec<WSOrderBookUpdateByBit>,
+    pub b: Vec<WSOrderBookUpdateByBit>,
+}
+
+impl HTTPSnapShotDepthResponseByBit {
+    pub fn depths(
+        self,
+        location: u8,
+    ) -> (
+        impl Iterator<Item = DepthUpdate>,
+        impl Iterator<Item = DepthUpdate>,
+    ) {
+        let bid_updates = self.data.b.into_iter().map(move |update| DepthUpdate {
+            k: 0,
+            p: update.price,
+            q: update.quantity,
+            l: location,
+        });
+
+        let ask_updates = self.data.a.into_iter().map(move |update| DepthUpdate {
+            k: 1,
+            p: update.price,
+            q: update.quantity,
+            l: location,
+        });
+
+        (bid_updates, ask_updates)
+    }
+}
+
+#[allow(non_snake_case)]
+#[derive(Debug, Serialize, Deserialize)]
+pub struct HTTPSnapShotDepthResponseByBit {
+    pub retMsg: Option<String>,
+    pub topic: String,
+    pub ts: u64,
+    pub r#type: String,
+    pub data: ByBitData,
 }
 
 #[cfg(test)]
--- orderbook-quoter-server/src/main.rs
+++ orderbook-quoter-server/src/main.rs
@@ -28,38 +28,38 @@ use tracing_subscriber;
 
 fn main() {
     tracing_subscriber::fmt::init();
-    info!("starting orderbook-quoter-server");
-    let current_dir_result = env::current_dir();
-    match current_dir_result {
-        Ok(mut file) => {
-            file.push("quoter-config.yaml");
-            let config_file = read_yaml_config(file);
-            info!("found configuration file");
-            let num_cpus = num_cpus::get();
-            let mut async_rt = Builder::new_multi_thread()
-                .enable_io()
-                .enable_time()
-                .worker_threads(usize(
-                    f64(num_cpus) * config_file.as_ref().unwrap().io_thread_percentage,
-                ))
-                .build()
-                .unwrap();
-            let mut tp = rayon::ThreadPoolBuilder::new()
-                .num_threads(usize(
-                    f64(num_cpus)
-                        - (f64(num_cpus) * config_file.as_ref().unwrap().io_thread_percentage),
-                ))
-                .build()
-                .unwrap();
-            let res = orderbook_quoter_server(&config_file.unwrap(), &mut async_rt, &mut tp);
-            match res {
-                Ok(_) => info!("shutting down orderbook_quoter_server"),
-                Err(error) => error!("failed to run orderbook quoter server {}", error),
-            }
-        }
-        Err(error) => {
-            error!("failed to start orderbook quoter server: {}", error);
-        }
+    let matches = App::new("orderbook quoter server")
+        .arg(
+            Arg::new("config")
+                .long("config")
+                .value_name("FILE")
+                .takes_value(true),
+        )
+        .get_matches();
+    let config_path = PathBuf::from(matches.value_of("config").unwrap_or("/etc/config.yaml"));
+    info!("config path given: {:?}", config_path);
+    let file = PathBuf::from(config_path);
+    let config_file = read_yaml_config(file);
+    info!("config loaded succesfully");
+    let num_cpus = num_cpus::get();
+    let mut async_rt = Builder::new_multi_thread()
+        .enable_io()
+        .enable_time()
+        .worker_threads(usize(
+            f64(num_cpus) * config_file.as_ref().unwrap().io_thread_percentage,
+        ))
+        .build()
+        .unwrap();
+    let mut tp = rayon::ThreadPoolBuilder::new()
+        .num_threads(usize(
+            f64(num_cpus) - (f64(num_cpus) * config_file.as_ref().unwrap().io_thread_percentage),
+        ))
+        .build()
+        .unwrap();
+    let res = orderbook_quoter_server(&config_file.unwrap(), &mut async_rt, &mut tp);
+    match res {
+        Ok(_) => info!("shutting down orderbook_quoter_server"),
+        Err(error) => error!("failed to run orderbook quoter server {}", error),
     }
 }
 
@@ -68,6 +68,7 @@ fn orderbook_quoter_server(
     async_runtime: &mut Runtime,
     thread_pool: &mut ThreadPool,
 ) -> Result<(), Box<dyn Error>> {
+    info!("starting orderbook-quoter-server");
     let async_handle = async_runtime.handle();
     // let (orderbook, orders_producer) = OrderBook::new(&config.orderbook);
     let (orderbook_producer, _) = bounded(4);
@@ -77,17 +78,17 @@ fn orderbook_quoter_server(
     let handle = async_handle.block_on(async move {
         exchange_controller.boot_exchanges().await;
         async_sleep(async_duration::from_secs(10)).await;
-        while let Some(mut exchange_stream) = exchange_controller.exchange() {
+
+        let mut exchange_handles = Vec::new();
+        while let Some(mut exchange_stream) = exchange_controller.pop_exchange() {
             info!("spawning async handler for");
-            tokio::spawn(async move {
+            let exchange_handler = tokio::spawn(async move {
                 exchange_stream.next().await;
             });
+            exchange_handles.push(exchange_handler);
         }
+        let _ = join_all(exchange_handles).await;
     });
-
-    async_runtime.shutdown_timeout(Duration::from_millis(100));
-
-    // TODO: wait for tasks to finish
 
     info!("quoter server is shutting down");
     Ok(())
