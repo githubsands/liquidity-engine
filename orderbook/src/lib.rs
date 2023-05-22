2e115cef69078ea4799ec552e19bfdc5ac0c86c1
Ryan Vacek <ryanvacek@Ryans-MacBook-Pro.local>
2023-05-22 16:52:13 -0600

    adds quoting from orderbook

--- orderbook/src/lib.rs
+++ orderbook/src/lib.rs
@@ -16,6 +16,8 @@ use typed_arena::Arena;
 
 use std::f64::NAN;
 
+use thin_vec::ThinVec;
+
 use market_object::DepthUpdate;
 use quoter_errors::ErrorHotPath;
 
@@ -75,14 +77,16 @@ impl<T> Drop for Stack<T> {
 
 struct OrderBook<'a> {
     exchange_count: usize,
+    level_increment: f64, // level increment is the basis points for each of each depth difference
+    // for this asset
     best_deal_bids_level: f64,
     best_deal_asks_level: f64,
     asks: FxHashMap<OrderedFloat<f64>, Stack<VolumeNode<'a>>>,
-    bids: FxHashMap<OrderedFloat<f64>, Stack<VolumeNode<'a>>>,
+    pub bids: FxHashMap<OrderedFloat<f64>, Stack<VolumeNode<'a>>>,
 }
 
 impl<'a> OrderBook<'a> {
-    fn new(exchange_count: usize, depth: usize, current_price: usize) -> OrderBook<'a> {
+    pub fn new(exchange_count: usize, depth: usize, current_price: usize) -> OrderBook<'a> {
         let ask_level_range: usize = current_price + depth;
         let mut asks = FxHashMap::<OrderedFloat<f64>, Stack<VolumeNode<'a>>>::default();
         for i in current_price..ask_level_range {
@@ -113,6 +117,7 @@ impl<'a> OrderBook<'a> {
         }
         let orderbook: OrderBook<'a> = OrderBook {
             exchange_count: exchange_count,
+            level_increment: 0.0,
             best_deal_bids_level: 0.0,
             best_deal_asks_level: 0.0,
             asks: asks,
@@ -224,6 +229,115 @@ impl<'a> OrderBook<'a> {
             _ => Err(ErrorHotPath::OrderBook),
         }
     }
+    pub fn get_quotes_bids(&mut self) -> ThinVec<Deal> {
+        let mut deals = ThinVec::new();
+        let mut current_seek: usize = 0;
+        let mut current_level = self.best_deal_asks_level;
+        let mut current_deal_index = 0;
+        while current_seek < self.exchange_count {
+            let node = self
+                .bids
+                .get(&OrderedFloat(current_level))
+                .unwrap()
+                .peek()
+                .unwrap();
+            // check if volume node in list is not dedicated to an exchange if so bump our
+            // current level
+            if node.location == 0 {
+                let _ = self
+                    .bids
+                    .get_mut(&OrderedFloat(self.best_deal_asks_level))
+                    .unwrap()
+                    .pop();
+                // update to the next level to check for deals. for the bid side we seek down the
+                // orderbook
+                current_level = current_level - self.level_increment;
+                continue;
+            }
+            deals.insert(
+                current_deal_index,
+                Deal {
+                    volume: node.volume,
+                    location: node.location,
+                    price: current_level,
+                },
+            );
+            current_deal_index = current_deal_index + 1;
+            current_seek = current_seek + 1;
+            _ = self
+                .bids
+                .get_mut(&OrderedFloat(self.best_deal_asks_level))
+                .unwrap()
+                .pop()
+        }
+        return deals;
+    }
+    pub fn get_quotes_asks(&mut self) -> ThinVec<Deal> {
+        let mut deals = ThinVec::new();
+        let mut current_seek: usize = 0;
+        let mut current_level = self.best_deal_asks_level;
+        let mut current_deal_index = 0;
+        while current_seek < self.exchange_count {
+            let node = self
+                .bids
+                .get(&OrderedFloat(current_level))
+                .unwrap()
+                .peek()
+                .unwrap();
+            // check if volume node in list is not dedicated to an exchange if so bump our
+            // current level
+            if node.location == 0 {
+                let _ = self
+                    .bids
+                    .get_mut(&OrderedFloat(self.best_deal_asks_level))
+                    .unwrap()
+                    .pop();
+                // update to the next level to check for deals. for asks side we seek up the
+                // orderbook
+                current_level = current_level + self.level_increment;
+                continue;
+            }
+            deals.insert(
+                current_deal_index,
+                Deal {
+                    volume: node.volume,
+                    location: node.location,
+                    price: current_level,
+                },
+            );
+            current_deal_index = current_deal_index + 1;
+            current_seek = current_seek + 1;
+            _ = self
+                .bids
+                .get_mut(&OrderedFloat(self.best_deal_asks_level))
+                .unwrap()
+                .pop()
+        }
+        return deals;
+    }
+    // TODO: get_quotes_$side should be ran in their own thread.
+    pub fn get_quote_bids(&mut self) -> Quotes {
+        let ask_deals = self.get_quotes_asks();
+        let bid_deals = self.get_quotes_bids();
+        // ask_deals should always be higher then bid deals. if not we have a problem
+        Quotes {
+            spread: ask_deals.get(0).unwrap().price - bid_deals.get(0).unwrap().price,
+            best_asks: ask_deals,
+            best_bids: bid_deals,
+        }
+    }
+}
+
+pub struct Deal {
+    volume: f64,
+    price: f64,
+    location: u8,
+}
+
+pub struct Quotes {
+    pub spread: f64,
+    pub best_bids: ThinVec<Deal>,
+    pub best_asks: ThinVec<Deal>,
 }
 
 /*
