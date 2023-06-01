d945d65a8595086765f36c78dfbed2780dfaf250
Ryan Vacek <ryanvacek@Ryans-MacBook-Pro.local>
2023-06-01 14:42:23 -0600

    adds bulk depth update to depth generator and adds binance type conversion to exchange stub

--- testing/depth-generator/src/lib.rs
+++ testing/depth-generator/src/lib.rs
@@ -32,7 +32,7 @@ impl DepthMessageGenerator {
             price_std,
         }
     }
-    pub fn depth_message(&mut self, location: u8) -> DepthUpdate {
+    pub fn depth_message_random(&mut self, location: u8) -> DepthUpdate {
         let mut kind: u8;
         let msg_type = match rand::random() {
             true => {
@@ -62,6 +62,42 @@ impl DepthMessageGenerator {
             l: location,
         }
     }
+    pub fn depth_message(&mut self, location: u8, is_ask: bool) -> DepthUpdate {
+        let kind: u8 = if is_ask { 0 } else { 1 };
+        let normal = Normal::new(0.0, 1.0).unwrap();
+        let volume_diff = normal.sample(&mut rand::thread_rng()) * self.vol_std;
+        let price_diff = normal.sample(&mut rand::thread_rng()) * self.price_std;
+        let volume = (self.volume + volume_diff).max(0.0);
+        let price = if is_ask {
+            self.price - price_diff
+        } else {
+            self.price + price_diff
+        };
+        DepthUpdate {
+            k: kind,
+            p: DepthMessageGenerator::round_to_hundreth(price as f64),
+            q: volume as f64,
+            l: location,
+        }
+    }
+    pub fn generate_depth_bulk(
+        &mut self,
+        location: u8,
+        size: u8,
+    ) -> (Vec<DepthUpdate>, Vec<DepthUpdate>) {
+        let asks = vec![0; size as usize];
+        let bids = vec![0; size as usize];
+        let asks: Vec<DepthUpdate> = asks
+            .iter()
+            .map(|_| self.depth_message(location, true))
+            .collect();
+        let bids: Vec<DepthUpdate> = bids
+            .iter()
+            .map(|_| self.depth_message(location, false))
+            .collect();
+        (asks, bids)
+    }
+
     fn round_to_hundreth(n: f64) -> f64 {
         (n * 100.0).round() / 100.0
     }
