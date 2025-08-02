e02b62b390ed1a9e52d1a90a287545acd275d799
ryan vacek <ryvace@protonmail.com>
2025-08-02 10:51:33 -0700

    cleans up market maker and removes unused code

--- bin/market-maker/decision-maker/src/lib.rs
+++
@@ -1,5 +0,0 @@
-pub struct DecisionMaker {}
-
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
--- bin/market-maker/src/main.rs
+++ bin/market-maker/src/main.rs
@@ -30,26 +30,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         println!("ORDERBOOK_QUOTER_SERVER_URI is not set");
         process::exit(0);
     }
-    /*
-    let mut exchange_server_one_uri: String = "".to_string();
-    if let Ok(exchange_server_one_uri) = env::var("EXCHANGE_SERVER_1_URI") {
-        println!("exchange server 1 uri is: {}", exchange_server_one_uri);
-    } else {
-        println!("exchange server 1 uri is not set");
-        process::exit(0);
-    }
-    let mut exchange_server_two_uri: String = "".to_string();
-    if let Ok(exchange_server_two_uri) = env::var("EXCHANGE_SERVER_2_URI") {
-        println!("exchange server 2 uri is: {}", exchange_server_two_uri);
-    } else {
-        println!("exchange server 2 uri is not set");
-        process::exit(0);
-    }
-    */
-    // let mut client = QuoterClient::connect(env::var("ORDERBOOK_QUOTER_SERVER_URI").unwrap())
-    //
     let mut client = QuoterClient::connect("http://[::1]:9090").await.unwrap();
-
     let market_maker = MarketMaker::new();
     market_maker.stream_quotes(&mut client, 50000).await;
     Ok(())
@@ -57,26 +38,12 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
 pub struct MarketMaker {
     agent: String,
-    // exchanges: Vec<Exchange>,
-    // quote_producer: Sender<QuoterResponse>,
-    // quote_consumer: Receiver<QuoterResponse>,
 }
 
 impl MarketMaker {
     fn new() -> Self {
-        /*
-        let (producer, consumer) = channel(1000);
-        let mut exchanges: Vec<Exchange> = vec![];
-        for uri in exchange_uris {
-            let exchange = Exchange::new(uri);
-            exchanges.push(exchange);
-        }
-        */
         MarketMaker {
             agent: "market-maker-strategy-tbd".to_string(),
-            //   exchanges,
-            // quote_producer: producer,
-            // quote_consumer: consumer,
         }
     }
     async fn stream_quotes(self, client: &mut QuoterClient<Channel>, num: usize) {
