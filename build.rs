79018b8da2a9d801da389dc28102a1b80fe5ef40
ryan vacek <ryvace@protonmail.com>
2025-08-02 09:47:21 -0700

    cleans up build script

--- build.rs
+++ build.rs
@@ -1,7 +1,5 @@
-use serde::{Deserialize, Serialize};
 use serde_yaml;
 use std::fs;
-use tonic_build::{compile_protos, configure};
 
 use config::Config;
 
@@ -14,11 +12,12 @@ struct ExchangeMacro {
 
 const CONFIG_LOCATION: &'static str = "integrative-testing-config.yaml";
 
+// todo: what was i doing here specifically?
 const EXCHANGE_MACROS: [ExchangeMacro; 1] = [ExchangeMacro {
     macro_name: "orderbook_levels",
     file_location: "orderbook/src/lib.rs",
-    search_pattern: "new_level!(6)",
-    replacement_value: "new_level!(N)",
+    search_pattern: "new_level!(2)",
+    replacement_value: "new_level!(10)",
 }];
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -28,26 +27,17 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let config: Config = serde_yaml::from_str(&config_content)?;
     assert!(config.exchanges.len() == config.orderbook.exchange_count as usize);
     let exchange_count = config.orderbook.exchange_count;
-
-    for mut replacements in EXCHANGE_MACROS {
+    for replacements in EXCHANGE_MACROS {
         if replacements.macro_name == "orderbook_levels" {
             replacements
                 .replacement_value
                 .replace("N", exchange_count.to_string().clone().as_str());
         }
-
         let file_path = current_dir.join(replacements.file_location);
-
-        // Read the file contents
         let mut file_contents = fs::read_to_string(&file_path).expect("Failed to read file");
-
-        // Perform the search and replace
         file_contents =
             file_contents.replace(replacements.search_pattern, replacements.replacement_value);
-
-        // Write the modified contents back to the file
         fs::write(&file_path, file_contents).expect("Failed to write file");
-
         println!(
             "Search and replace completed in file: {}",
             file_path.display()
