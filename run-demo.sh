2271a4d26a495907976fa1090e94e5fbc01d2014
ryan vacek <ryvace@protonmail.com>
2025-08-03 13:33:09 -0700

    updates run demo script

--- run-demo.sh
+++ run-demo.sh
@@ -1,5 +1,4 @@
-
-$(pwd)/target/debug/orderbook-quoter-server --config /etc/usa-config.json
-sleep 2
 export ORDERBOOK_QUOTER_SERVER_URI="[::1]:9090"
-$(pwd)/market-maker/target/debug/market-maker
+./bin/orderbook-quoter-server/debug/orderbook-quoter-server --config /etc/usa-config.json
+sleep 2
+./bin/market-maker/target/debug/market-maker
